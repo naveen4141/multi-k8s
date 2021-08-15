3a7b51d0fb5688aa4e82a9187b8da37352e1f279
naveen <ndavu@gmail.com>
2021-08-15 12:42:29 +0530

    gf

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t naveen4141/multi-client:latest -t naveen4141/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t naveen4141/multi-server:latest -t naveen4141/multi-serveri:$SHA -f ./server/Dockerfile ./server
+docker build -t naveen4141/multi-worker:latest -t naveen4141/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push naveen4141/multi-client:latest
+docker push naveen4141/multi-server:latest
+docker push naveen4141/multi-worker:latest
+
+ocker push naveen4141/multi-client:$SHA
+docker push naveen4141/multi-server:$SHA
+docker push naveen4141/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=naveen4141/multi-server:$SHA
+kubectl set image deployments/client-deployment client=naveen4141/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=naveen4141/multi-worker:$SHA
